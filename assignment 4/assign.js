819e88ccfd35d92a01225d0c0a9bcb0bfe7c5282
DexZed <144045080+DexZed@users.noreply.github.com>
2024-09-12 18:06:41 +0600

    "Updated assign.js: commented out checkDigitsInName function, added calculateFinalScore function, and added a console log statement to test the new function."

--- assignment 4/assign.js
+++ assignment 4/assign.js
@@ -10,10 +10,20 @@
 //     ? `${username} sent you an email from ${domainName}.`
 //     : "Invalid Email";
 // }
-function checkDigitsInName(name) {
-    return typeof name !== "string"
-      ? "Invalid Input"
-      : /[0-9]/.test(name) || /[^a-zA-Z\s]/.test(name)
-      ? false
-      : true;
-  }
+// function checkDigitsInName(name) {
+//     return typeof name !== "string"
+//       ? "Invalid Input"
+//       : /[0-9]/.test(name) || /[^a-zA-Z\s]/.test(name)
+//       ? false
+//       : true;
+//   }
+
+function calculateFinalScore( obj ) {
+  if (!obj || typeof obj.name !== "string" || typeof obj.testScore !== "number" || 
+    typeof obj.schoolGrade !== "number" || typeof obj.isFFamily !== "boolean") {
+  return "Invalid Input";
+}
+  let score = Math.max(0,Math.min(obj.testScore,50)) + Math.max(0,Math.min(obj.schoolGrade,30)) + (obj.isFFamily ? 20 : 0);
+  return score >=80; 
+}
+console.log(calculateFinalScore({ name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true  }));
