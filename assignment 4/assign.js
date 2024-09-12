2ab6b3399d3d1e39637a7cfdd0299eb36fa0ed48
DexZed <144045080+DexZed@users.noreply.github.com>
2024-09-12 08:53:19 +0600

    "Commented out existing functions calculateTax and sendNotification, added new function checkDigitsInName and a console log statement to test it."

--- assignment 4/assign.js
+++ assignment 4/assign.js
@@ -1,12 +1,19 @@
-function calculateTax(income, expense) {
-  return income <= 0 || expense < 0 || income < expense
-    ? "Invalid Input"
-    : (income - expense) * 0.2;
-}
+// function calculateTax(income, expense) {
+//   return income <= 0 || expense < 0 || income < expense
+//     ? "Invalid Input"
+//     : (income - expense) * 0.2;
+// }
 
-function sendNotification(email) {
-  let [username, domainName] = email.split("@");
-  return username && domainName
-    ? `${username} sent you an email from ${domainName}.`
-    : "Invalid Email";
-}
+// function sendNotification(email) {
+//   let [username, domainName] = email.split("@");
+//   return username && domainName
+//     ? `${username} sent you an email from ${domainName}.`
+//     : "Invalid Email";
+// }
+function checkDigitsInName(name = "zxc@#123") {
+    return typeof name !== "string"
+      ? "Invalid Input"
+      : /[0-9]/.test(name) || /[^a-zA-Z\s]/.test(name)
+      ? false
+      : true;
+  }
